fb1829db4ecb764a4daf60011e5ce099aef0b59e
Roy Motloutsi <roy@sticitt.co.za>
2018-10-07 20:57:04 +0200

    Use external redis instance

--- src/job/downloader.py
+++ src/job/downloader.py
@@ -5,8 +5,9 @@ sys.path.insert(0, './src')
 import redis
 from rq import Worker, Queue, Connection
 
-listen = ['high', 'default', 'low']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+listen = ['default']
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = 'redis://teris:Mz5nDwQVT9h3lQa0pEI3vYMKNZNnO1Nr@redis-10454.c44.us-east-1-2.ec2.cloud.redislabs.com:10454'
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
