4f43297ad04079df36d8fd13e59195ae71ab85a5
Roy Motloutsi <roy@sticitt.co.za>
2018-10-08 17:49:40 +0200

    Add fetch url

--- src/cmd/utils.py
+++ src/cmd/utils.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import os
 import requests
+import subprocess
 import youtube_dl
 
 ydl_opts = {
@@ -11,6 +12,18 @@ ydl_opts = {
 def count_words_at_url(url):
 	resp = requests.get(url)
 	return len(resp.text.split())
+
+def fetch_url(url, opts=''):
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
+    data = ydl.extract_info(url, download=False)
+    cmd = ['youtube-dl', '-g', '-q', '--no-check-certificate', url]
+    cmd.extend(opts.split())
+    result = subprocess.check_output(cmd)
+    return {
+        'url': result.decode('utf-8'),
+        'title': data['title'],
+        'ext': data['ext'],
+    }
 
 def fetch_video(url, opts=ydl_opts):
 	# merge the opts
