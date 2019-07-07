35bb09e28f99816237b31cf8e7665bb56e9c2afe
Roy Motloutsi <roy.motloutsi@gmail.com>
2019-07-07 13:24:17 +0200

    Update redis connection string

--- src/job/downloader.py
+++ src/job/downloader.py
@@ -5,6 +5,5 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-# redis_url = 'redis://teris:Mz5nDwQVT9h3lQa0pEI3vYMKNZNnO1Nr@redis-10454.c44.us-east-1-2.ec2.cloud.redislabs.com:10454'
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
